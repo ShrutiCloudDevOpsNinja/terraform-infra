9307c46f5b3b2bf16d14a12fab9e47e0aa81e17e
Shruti Grover <shruti.grover96@gmail.com>
2025-12-28 19:21:49 +0530

    Update provider.tf

--- Env/prod/provider.tf
+++ Env/prod/provider.tf
@@ -5,9 +5,10 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "myResourceGroup"
-    storage_account_name = "shrutistorageacct12345"
+
+backend "azurerm" {
+    resource_group_name  = "shruti-rg"
+    storage_account_name = "shrutiaccount2608"
     container_name       = "mycontainer"
     key                  = "shruti.tfstate"
 
